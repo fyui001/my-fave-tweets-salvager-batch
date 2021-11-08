9f6bdde5e4d41ee15f5915114a8b8c0ac39ef1b6
fyui001 <yun.cod@gmail.com>
2021-11-09 04:42:00 +0900

    DB・File周りの期間をinfra層に

---
+++ src/infra/File/csv.py
@@ -0,0 +1,15 @@
+import csv
+from src.domain.Twitter.TweetList import TweetList
+
+
+class Csv:
+    def __init__(self, file_path: str, file_name: str) -> None:
+        self.file_path = file_path
+        self.file_name = file_name
+
+    def create(self, data: TweetList) -> None:
+        with open(self.file_path + self.file_name, 'w', newline='') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            for row in data.get_value():
+                writer.writerow(row)
+            f.close()
