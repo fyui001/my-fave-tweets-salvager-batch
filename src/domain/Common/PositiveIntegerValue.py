0bd11a5cd25cbf6127a40e4b33fe97302816a200
fyui001 <yun.cod@gmail.com>
2021-11-09 23:07:57 +0900

    bug fix

--- src/domain/Common/PositiveIntegerValue.py
+++ src/domain/Common/PositiveIntegerValue.py
@@ -1,6 +1,9 @@
 class PositiveIntegerValue:
-    def __init__(self, value: str) -> None:
+    def __init__(self, value: int) -> None:
+        if value < 0:
+            raise
+
         self.__value = value
 
-    def get_value(self) -> str:
+    def get_value(self) -> int:
         return self.__value
